ee6db6b0b8f7e2ef5e244338922c90c0162e7757
Ramsai <ramsaicrony@gmail.com>
2023-02-10 11:26:28 +0530

    Migrated to ES module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 const app = express();
+
+ import HashtagRepository from './repository/hashtag-repository.js';
+ import service from './services/tweet-service.js';
 
 app.listen(3000, async () => {
     console.log("Server started");
     await connect();
     console.log('Mongo db connected');
+    let ser = new service();
+    await ser.create({content: 'Done with #refractor ?'});
 });
