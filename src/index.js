0d568c05ff24759bbbae8da9a37d3157bac61973
Ramsai <ramsaicrony@gmail.com>
2023-02-13 11:16:31 +0530

    Toggle feature working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import {connect} from './config/database.js';
 import apiRoutes from './routes/index.js';
-import bodyParser from 'body-parser';
+
+import { UserRepository, TweetRepository } from './repository/index.js';
+import LikeService from './services/like-service.js';
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,4 +17,11 @@ app.listen(3000, async () => {
     console.log("Server started");
     await connect();
     console.log('Mongo db connected');
+
+    const userRepo = new UserRepository();
+    const tweetRepo = new TweetRepository();
+    const tweets = await tweetRepo.getAll(0, 10);
+    const users = await userRepo.getAll();
+    const likeService = new LikeService();
+    await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
 });
